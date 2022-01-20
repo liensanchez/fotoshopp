3eee19e814e0687e05b60892237475a12a6565f7
Lien! <liensilviosanchez@gmail.com>
2022-01-19 23:55:37 -0300

    login

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ registrarse.addEventListener('click', function(){
   <input placeholder="numero" type="number" id="inputNumero"></input>
   <button type="sumbit" id="botonRegistro">Registrarse!</button>
   `;
-  //estilo form
-  
+
   //agrego el form utilizo loginBox. en lugar de document.boy ya que necesito agregarlo adento de ese div
   loginBox.appendChild(registro);
   //envio del formulario
@@ -20,14 +19,50 @@ registrarse.addEventListener('click', function(){
       //evitaoms que se refresque la pagina
       event.preventDefault();
       //obtenemos los datos del form
-        let nombre=document.getElementById('inputNombre').value;
-        let apellido=document.getElementById('inputApellido').value;
-        let telefono=document.getElementById('inputNumero').value;      
+        let nombre = document.getElementById('inputNombre').value;
+        let apellido = document.getElementById('inputApellido').value;
+        let telefono = document.getElementById('inputNumero').value;
+      //los guardamos en el Storage      
+      localStorage.setItem('nombre', nombre);
+      localStorage.setItem('apellido', apellido);
+      localStorage.setItem('numero', telefono);
     })
-  
 })
+//ingreso de un cliente
+let ingresar = document.getElementById('ingresar');
+ingresar.addEventListener('click', function(){
+  //tomamos los datos para compararlos con los del storage
+  let usuario = document.createElement('form');
+  //editamos el form
+    usuario.innerHTML=`
+    <input placeholder="nombre" type="text" id="inputNombre"></input>
+    <input placeholder="numero" type="number" id="inputNumero"></input>
+    <button type="sumbit" id="botonIngreso">Ingresar!</button>
+    `;
+    //agregamos el form
+    loginBox.appendChild(usuario);
+    //enviamos el formulario
+      botonIngreso.addEventListener('click', function(event){
+        //evitamos el refresco de la pagina
+        event.preventDefault();
+        //obtenemos los datos del form
+        let nombre = document.getElementById('inputNombre').value;
+        let telefono = document.getElementById('inputNumero').value;
+        //los comparamos con lo almacenado en el storage
+        if ((nombre == localStorage.nombre)&&(telefono == localStorage.telefono)) {
+          alert("Bienvenido "+localStorage.nombre);
+        }
+          else if (nombre != localStorage.nombre){
+            alert("Usted no se registro correctamente");
+          }
+          else{
+            alert("Usted debe registrarse")
+          }
+        //corroboramos el numero de telefono
+        if (telefono != localStorage.telefono){
+          alert("el numero de telefono no coincide");
+        }
+      })
 
-
-
-//creador de usuarios
+})
 
